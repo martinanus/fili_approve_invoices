4f014af33a17fbd79f7db16a7581eb87af43af5f
Martin <manus@fi.uba.ar>
2023-06-22 20:59:39 -0300

    adds datetime declararion to query

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from google.oauth2 import service_account
 key_path = "credentials.json"
 
 
-def get_client():
+def get_client():    
     credentials = service_account.Credentials.from_service_account_file(
         key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
     )
@@ -14,12 +14,12 @@ def get_client():
     return client;
 
 
-def update_big_query(id, dataset):
+def update_big_query(id, dataset, datetime):
     client = get_client();
 
     query = """
     DECLARE
-      today DATE DEFAULT CURRENT_DATE();
+      today DATE DEFAULT CURRENT_DATE('"""+str(datetime)+"""');
     UPDATE
       `fili-377220."""+str(dataset)+""".invoices`
     SET
@@ -28,7 +28,7 @@ def update_big_query(id, dataset):
       pay_delay = DATE_DIFF(today, due_date, DAY),
       days_to_pay = null
     WHERE
-      unique_key="""+str(id);
+      unique_key='""" +str(id) + """'""";
 
     query_job = client.query(query)  # Make an API request.
 
@@ -37,18 +37,19 @@ def update_big_query(id, dataset):
 def main(request):
     request_args = request.args
 
-    exp_args = ['id', 'invoiceid', 'installment', 'dataset']
+    exp_args = ['id', 'invoiceid', 'installment', 'dataset', 'datetime']
     act_args = list(request_args.keys())
 
     if request_args and set(exp_args).issubset(act_args):
-       id             = request_args['id']
-       invoiceid      = request_args['invoiceid']
-       installment      = request_args['installment']
-       dataset      = request_args['dataset']
-       update_big_query(id, dataset);
-       return ('La cuota {} de la factura con ID {} fue marcada como paga!'.format(installment, invoiceid)
+       id             = request_args['id']       
+       invoiceid      = request_args['invoiceid']       
+       installment      = request_args['installment']       
+       dataset      = request_args['dataset']       
+       datetime      = request_args['datetime']       
+       update_big_query(id, dataset, datetime);
+       return ('¡La cuota {} de la factura con ID {} fue marcada como paga!'.format(installment, invoiceid)
                 + '\n\n Puede cerrar esta ventana.')
     else:
-       return ('Error en la identificación de la factura seleccionada. '
+       return ('Error en la identificación de la factura seleccionada. ' 
               + '\n\n Por favor, contacte al administrador para resolver el problema.')
 
